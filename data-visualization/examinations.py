0c89206abb6b1a663f26a8ce35cc0ea8114aa233
ArianaK4 <ariana.kadriu@student.uni-pr.edu>
2023-01-27 00:14:51 +0100

    added examinations charts and filtering

--- data-visualization/examinations.py
+++ data-visualization/examinations.py
@@ -32,3 +32,59 @@ def show():
     if examination_type != 'All':
         filteredDF = examinationsDF[(examinationsDF['ExaminationType'] == examination_type) & (examinationsDF['ExaminationResult'].isin(examination_result) if examination_result else True) & (
             examinationsDF['RegistrationDate'] >= dates[0].strftime('%Y-%m-%d')) & (examinationsDF['RegistrationDate'] <= dates[1].strftime('%Y-%m-%d'))]
+
+    filteredDF['yearOnly'] = filteredDF['RegistrationDate']
+    for row, col in filteredDF.iterrows():
+        filteredDF.loc[row, 'yearOnly'] = col['yearOnly'].split(
+            '-')[0]
+
+    filteredDF['DateBin'] = filteredDF['RegistrationDate']
+    for row, col in filteredDF.iterrows():
+        filteredDF.loc[row, 'DateBin'] = col['DateBin'].split(
+            '-')[0] + '-' + col['DateBin'].split('-')[1]
+
+    st.subheader("Number of examination types along the years")
+    chart = alt.Chart(filteredDF).mark_circle().encode(
+        x='yearOnly',
+        y='count()',
+        color='ExaminationType',
+        size='ExaminationType'
+    ).interactive()
+
+    st.altair_chart(chart, use_container_width=True)
+
+    st.subheader("Number of examination results along the years")
+    reschart = alt.Chart(filteredDF).mark_circle().encode(
+        x='yearOnly',
+        y='count()',
+        color='ExaminationResult',
+        size='ExaminationResult'
+    ).interactive()
+
+    st.altair_chart(reschart, use_container_width=True)
+
+    st.subheader("Number of examination types and results are the same")
+    resTypechart = alt.Chart(filteredDF).mark_circle().encode(
+        x='ExaminationType',
+        y='count()',
+        color='ExaminationResult',
+        size='ExaminationResult'
+    ).interactive()
+
+    st.altair_chart(resTypechart, use_container_width=True)
+
+    st.subheader("Number of times each examination type has been done")
+    type_bars = alt.Chart(filteredDF).mark_bar(color='#33FFB5').encode(
+        x="ExaminationType",
+        y="count()"
+    ).interactive()
+
+    st.altair_chart(type_bars, use_container_width=True)
+
+    st.subheader("Number of times each examination result has been the same")
+    result_bars = alt.Chart(filteredDF).mark_bar(color='#ff6666').encode(
+        x="ExaminationResult",
+        y="count()"
+    ).interactive()
+
+    st.altair_chart(result_bars, use_container_width=True)
