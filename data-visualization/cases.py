9fbce98552a116ee8d6d0695b63f4714942da4d9
edonapllana <edona.pllana0@gmail.com>
2023-01-27 00:20:06 +0100

    added cases charts as part of dv

--- data-visualization/cases.py
+++ data-visualization/cases.py
@@ -40,3 +40,70 @@ def show():
             '%Y-%m-%d')) & (casesDF['RegistrationDate'] <= dates[1].strftime('%Y-%m-%d'))]
     if len(risk_factors_selected) > 0:
         filteredDF = filteredDF[filteredDF[risk_factors_selected].any(axis=1)]
+
+    filteredDF['DateBin'] = filteredDF['RegistrationDate']
+    for row, col in filteredDF.iterrows():
+        filteredDF.loc[row, 'DateBin'] = col['DateBin'].split(
+            '-')[0] + '-' + col['DateBin'].split('-')[1]
+
+    filteredDF['yearOnly'] = filteredDF['RegistrationDate']
+    for row, col in filteredDF.iterrows():
+        filteredDF.loc[row, 'yearOnly'] = col['yearOnly'].split(
+            '-')[0]
+
+    st.subheader("Number of pulmonar and ex-pulmonar along the years")
+    filteredDFClass = filteredDF[(filteredDF['Class']) != '0']
+    treatmentChart = alt.Chart(filteredDFClass).mark_area().encode(
+        x='DateBin',
+        y='count()',
+        color='Class',
+    ).interactive()
+
+    st.altair_chart(treatmentChart, use_container_width=True)
+
+    st.subheader("Placement along the years")
+    plChart = alt.Chart(filteredDF).mark_circle().encode(
+        x='yearOnly',
+        y='count()',
+        color='Placement',
+    ).interactive()
+
+    st.altair_chart(plChart, use_container_width=True)
+
+    st.subheader("Risk factors along the years")
+    rfChart = alt.Chart(filteredDF).mark_circle().encode(
+        x='DateBin',
+        y='count()',
+        color='RiskFactors',
+    ).interactive()
+
+    st.altair_chart(rfChart, use_container_width=True)
+
+    st.subheader("Raport of pulmonar and ex-pulmonar")
+    filteredDFClass = filteredDF[(filteredDF['Class']) != '0']
+    placementChart = alt.Chart(filteredDFClass).mark_bar(color='#FF5733').encode(
+        x='Class',
+        y='count()',
+    ).interactive()
+
+    st.altair_chart(placementChart, use_container_width=True)
+
+    st.subheader("Number of placements")
+    filteredDFPlacement = filteredDF[(filteredDF['Placement']) != '0']
+    Placement_bars = alt.Chart(filteredDFPlacement).mark_bar().encode(
+        x="Placement",
+        y="count()",
+        color='Placement',
+    ).interactive()
+
+    st.altair_chart(Placement_bars, use_container_width=True)
+
+    st.subheader("Number of risk factors")
+    filteredDFRiskFactor = filteredDF[(filteredDF['RiskFactors']) != 'None']
+    riskChart = alt.Chart(filteredDFRiskFactor).mark_bar().encode(
+        x='RiskFactors',
+        y='count()',
+        color='RiskFactors',
+    ).interactive()
+
+    st.altair_chart(riskChart, use_container_width=True)
